a992c5a2cb04b6409f6af260aea9f43943a50f69
RonnyRozo <134233689+RonnyRozo@users.noreply.github.com>
2023-07-02 19:34:35 -0500

    Add files via upload

---
+++ Cajero.js
@@ -0,0 +1,7 @@
+alert('Bienvenido a Ronny Bank, realizaremos una validacion adicional antes de continuar');
+//Entradas: informacion a recibir//
+
+const name = prompt('Por favor ingresa tu nombre para continuar');
+
+
+
